7b2f186f1c155749b5b4cb01485d890e181eca2b
Akinseye Abayomi <abayomi.akinseye@gmail.com>
2023-03-14 09:26:12 +0100

    Initial Commit

---
+++ SwiftfulContinuedLearning/SwiftfulContinuedLearningApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulContinuedLearningApp.swift
+//  SwiftfulContinuedLearning
+//
+//  Created by Yomi on 3/14/23.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulContinuedLearningApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
